4378a1190f5421f3b969d533dd469e67c0a1af68
Igor Silva <51680417+igorsilvadev@users.noreply.github.com>
2021-10-08 14:38:28 -0300

    feat: autolayout programmatically app

--- RegisterBloodGlucose/ViewController.swift
+++ RegisterBloodGlucose/ViewController.swift
@@ -7,13 +7,81 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    lazy var inputGlucoseLevel: UITextField = {
+        let input = UITextField(frame: view.frame)
+        input.keyboardType = .numberPad
+        input.placeholder = "Enter your blood glucose level"
+        input.delegate = self
+        return input
+    }()
+    
+    lazy var saveButton: UIButton = {
+        let button = UIButton(frame: view.frame)
+        button.configuration = .plain()
+        button.setTitle("Save", for: .normal)
+        button.setTitleColor(.label, for: .normal)
+        button.backgroundColor = .systemBlue
+        button.addTarget(self, action: #selector(saveBloodGlucoseLevel(_:)), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    
+    lazy var bloodGlucoseLevel: UITextView = {
+        let outputText = UITextView()
+        outputText.contentInsetAdjustmentBehavior = .automatic
+        outputText.textAlignment = NSTextAlignment.center
+        outputText.font = .systemFont(ofSize: 50)
+        outputText.text = ""
+        outputText.textColor = .black
+        return outputText
+    }()
+    
+    
+    
     override func viewDidLoad() {
+        view.addSubview(inputGlucoseLevel)
+        view.addSubview(saveButton)
+        view.addSubview(bloodGlucoseLevel)
+        setupConstraints()
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    
+    
+    @objc func saveBloodGlucoseLevel(_ sender: UIButton!){
+        if let result = self.inputGlucoseLevel.text {
+            bloodGlucoseLevel.text = result
+        }
+    }
+    
+    
+    private func setupConstraints(){
+        self.inputGlucoseLevel.translatesAutoresizingMaskIntoConstraints = false
+        self.saveButton.translatesAutoresizingMaskIntoConstraints = false
+        self.bloodGlucoseLevel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let contraints = [
+            //Input Glucose Level
+            self.inputGlucoseLevel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            self.inputGlucoseLevel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            //Save Button
+            self.saveButton.centerXAnchor.constraint(equalTo: self.inputGlucoseLevel.centerXAnchor),
+            self.saveButton.centerYAnchor.constraint(equalTo: self.inputGlucoseLevel.centerYAnchor, constant: 50),
+            
+            //Blood Glucose Result View
+            self.bloodGlucoseLevel.centerXAnchor.constraint(equalTo: self.inputGlucoseLevel.centerXAnchor),
+            self.bloodGlucoseLevel.centerYAnchor.constraint(equalTo: self.inputGlucoseLevel.centerYAnchor, constant: -150),
+            self.bloodGlucoseLevel.widthAnchor.constraint(equalTo: self.inputGlucoseLevel.widthAnchor, constant: 50),
+            self.bloodGlucoseLevel.heightAnchor.constraint(equalTo: self.inputGlucoseLevel.heightAnchor, constant: 50)
+            
+        ]
+        NSLayoutConstraint.activate(contraints)
+    }
 }
 
